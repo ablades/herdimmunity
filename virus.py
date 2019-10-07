cf99c081b1817c090f6d09cbd6c43f4f56577410
ablades <audaris@gmail.com>
2019-10-07 16:06:15 -0700

    add virus test and logger test

--- virus.py
+++ virus.py
@@ -8,9 +8,13 @@ class Virus(object):
 
 
 def test_virus_instantiation():
-    #TODO: Create your own test that models the virus you are working with
     '''Check to make sure that the virus instantiator is working.'''
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+
+    virus2 = Virus("pox", 0.3, 0.7)
+    assert virus2.name == "pox"
+    assert virus2.repro_rate == 0.3
+    assert virus2.mortality_rate == 0.7
